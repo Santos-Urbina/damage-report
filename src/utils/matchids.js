bb59cb3adf0d4a5b1a1d9efd13c80575a7565ff9
Alex Santos <alexn.santos18@gmail.com>
2021-12-14 17:49:25 -0500

    Create module that uses account data to retrieve recent match IDs from Riot API

---
+++ src/utils/matchids.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const matchIds = (puuid, callback) => {
+    const url = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=RGAPI-0d5e2465-f2ab-4409-a793-d48a31ddfd8d`;
+
+    request({ url, json: true }, (error, { body }) => {
+        if(error)
+            callback('placeholder');
+        else if(body.length === 0)
+            callback('placeholder');
+        else {
+            callback(undefined, {
+                matchIds: body
+            });
+        }
+    });
+};
+
+module.exports = matchIds;
