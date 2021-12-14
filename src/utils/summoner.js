a83af39df18c470177f661d85426ea284e439dcf
Alex Santos <alexn.santos18@gmail.com>
2021-12-14 17:48:21 -0500

    create module to get account data from riot api

---
+++ src/utils/summoner.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const summoner = (summonerName, callback) => {
+    const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=RGAPI-0d5e2465-f2ab-4409-a793-d48a31ddfd8d`;
+
+    request({ url, json: true }, (error, { body }) => {
+        if(error)
+            callback('placeholder');
+        else if(body.length === 0)
+            callback('placeholder');
+        else {
+            callback(undefined, {
+                puuid: body.puuid,
+                summonerLevel: body.summonerLevel,
+                name: body.name
+            });
+        }
+    });
+};
+
+module.exports = summoner;
